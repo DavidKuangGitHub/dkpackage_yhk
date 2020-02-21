f1d31bdc2506d4e1ea7e05fddf9dccd4abd5bbe6
DavidKuangGitHub <davidkuang416@gmail.com>
2020-02-21 17:36:29 -0500

    Updated dkpackage.py Version0.03
    Updated dkpackage.py Version0.03

--- dkpackage/dkpackage.py
+++ dkpackage/dkpackage.py
@@ -1,4 +1,13 @@
-def start():
-    print("DKpackage: Started!")
-def stop():
-    print("DKpackage: Stopped!!")
+'''dkpackage.py'''
+
+
+class Davidsolution(object):
+    def start(self, strs):
+        return strs + "[thread:START]"
+
+    def stop(self, strs):
+        return strs + "[thread:STOP]"
+
+
+'''myDavidSolution = Davidsolution()
+print(myDavidSolution.start("DKpackage: Started!")+" and "+myDavidSolution.stop("DKpackage: Stopped!"))'''
